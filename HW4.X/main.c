fc3869c25c940b7682f7c9a9d181c523e9b83952
anapervan <b6d1g3@u.northwestern.edu>
2017-04-11 09:36:06 -0500

    Progress on HW4
    still not working though

---
+++ HW4.X/main.c
@@ -0,0 +1,137 @@
+#include <xc.h>       
+#include <stdio.h>
+#include <math.h>
+// Demonstrates spi
+// PIC is the master, MCP4092 is the slave
+// SDO1 -> SDI (pin A1 -> pin 5)
+// SDI1 -> SO (pin A1 -> none)
+// SCK1 -> SCK (pin B14 -> pin 4)
+// SS1 -> CS (pin A4 -> pin 3)
+
+// Additional SRAM connections
+// Vss (Pin 12) -> ground
+// Vdd (Pin 1) -> 3.3 V
+// 
+// Only uses the SRAM's sequential mode
+//
+#define CS LATAbits.LATA4      			    // chip select pin
+#define NUMSAMPS 100	   					// number of points in waveform
+char WaveformA[NUMSAMPS], WaveformB[NUMSAMPS]; // global variables
+
+void initSPI1();
+void SPI1_IO(unsigned char);
+void makeWaveformA();
+void makeWaveformB();
+void setVoltage(char, char);
+
+// initialize spi1
+void initSPI1() {
+  // set up the chip select pin as an output
+  // when a command is beginning (clear CS to low) and when it is ending (set CS high)
+  TRISAbits.TRISA4 = 0;
+  CS = 1;
+
+  // Master - SPI1, pins are: SDI1(A1), SDO1(A4), SCK1(B14).  
+  // we manually control SS1 as a digital output (B3)
+  // since the pic is just starting, we know that spi is off. We rely on defaults here
+ 
+  // setup SPI1
+  SPI1CON = 0;              // turn off the spi module and reset it
+  SPI1BUF;                  // clear the rx buffer by reading from it
+  SPI1CONbits.MODE32 = 0;   // send 8 bits of data per transfer
+  SPI1CONbits.MODE16 = 0;   // send 8 bits of data per transfer
+  SPI1BRG = 0x61A7;         // baud rate to 1 kHz [SPI4BRG = (50000000/(2*1000))-1 = 24999]
+  SPI1STATbits.SPIROV = 0;  // clear the overflow bit
+  SPI1CONbits.CKE = 1;      // data changes when clock goes from hi to lo (since CKP is 0)
+  SPI1CONbits.MSTEN = 1;    // master operation
+  SPI1CONbits.ON = 1;       // turn on spi 1
+  
+  RPA1Rbits.RPA1R = 0b0011;     // SDO pin
+  SDI1Rbits.SDI1R = 0b0100;     // SDI pin
+  SS1Rbits.SS1R = 0b0000;       // SS1 on A0
+}
+
+// send a byte via spi and return the response
+void SPI1_IO(unsigned char sent_data) {
+    unsigned char received_data;
+  SPI1BUF = sent_data;
+  received_data = SPI1BUF;
+  while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
+    ;
+  }
+}
+
+void makeWaveformA() {
+	int i = 0;								// sine wave
+	for (i = 0; i < NUMSAMPS; ++i) {
+		WaveformA[i] = 255*sin((2*3.14159/100)*i);
+		}
+	}
+
+void makeWaveformB() {
+	int i = 0;		// square wave, fill in center value and amplitude
+	for (i = 0; i < NUMSAMPS; ++i) {
+		WaveformB[i] = 2.55*i;
+	}
+}
+
+void setVoltage(char channel, char voltage) {
+// where channel is 0 or 1 (for VoutA and VoutB), and voltage is the 8-bit output level.
+unsigned char byte1, byte2;
+if (channel == 0) {
+	byte1 = (voltage >> 4) + 0b01110000; // right shift voltage 4 spaces and add 0111 at the beginning	
+}
+if (channel == 1) {
+	byte1 = (voltage >> 4) + 0b11110000; // right shift voltage 4 spaces and add 1111 at the beginning
+}
+byte2 = (voltage << 4); 			// left shift voltage 4 spaces
+
+CS = 0;
+SPI1_IO(byte1);
+SPI1_IO(byte2);
+CS = 1;
+}
+
+int main(void) {
+    
+     __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISBbits.TRISB4 = 1; // make push button an input
+    TRISAbits.TRISA4 = 0; // make green LED an output
+    LATAbits.LATA4 = 1; // set green LED initially high
+
+    __builtin_enable_interrupts();
+    
+  int i = 0;
+ 
+  initSPI1();
+  makeWaveformA();
+  makeWaveformB();
+  
+  while(1) {
+	  for (i = 0; i < NUMSAMPS; ++i) {
+		setVoltage(0,WaveformA[i]);
+		setVoltage(1,WaveformB[i]);
+	  
+	  _CP0_SET_COUNT(0);
+		while((_CP0_GET_COUNT()) < 40){
+			 // delay
+		}
+		
+	  }
+  }
+  return 0;
+}
