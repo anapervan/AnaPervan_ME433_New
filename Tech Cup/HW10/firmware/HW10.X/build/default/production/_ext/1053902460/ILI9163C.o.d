83b8e9797623e00a6444e344cb8ac6224db47ba8
anapervan <b6d1g3@u.northwestern.edu>
2017-06-02 12:32:43 -0500

    Working on final project

---
+++ Tech Cup/HW10/firmware/HW10.X/build/default/production/_ext/1053902460/ILI9163C.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1053902460/ILI9163C.o:  \
+ ../../../../../AnaPervan_ME433/HW9/firmware/src/ILI9163C.c  \
+ ../../../../../AnaPervan_ME433/HW9/firmware/src/ILI9163C.h 
---
+++ Tech Cup/HW10/firmware/HW10.X/build/default/production/_ext/1053902460/i2c_master.o.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1053902460/i2c_master.o:  \
+ ../../../../../AnaPervan_ME433/HW9/firmware/src/i2c_master.c  \
+ ../../../../../AnaPervan_ME433/HW9/firmware/src/i2c_master.h 
