d46b8a4f3421d2052a5819befda84e51b26cf3e8
anapervan <b6d1g3@u.northwestern.edu>
2017-06-08 09:17:22 -0500

    the motors move and respond to inputs in the terminal!

---
+++ HW18/HW18/build/default/production/_ext/612888211/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/612888211/main.o:  \
+ ../../HW14/firmware/src/main.c  \
+ ../../../../../../../../microchip/harmony/v1_10/framework/system/common/sys_module.h  \
+ ../../../../../../../../microchip/harmony/v1_10/framework/system/common/sys_common.h 
